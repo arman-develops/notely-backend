fdb438842dd7741cfc6d3cc4723787c2a0923c18
Amon Victor <amon.victor.develops@gmail.com>
2025-07-28 11:25:01 +0300

    Added origin cors option

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "https://notely-app-tau.vercel.app"
 }))
 app.use(json())
 app.use(indexRouter)
