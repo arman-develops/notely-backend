d4daeb5bf679a6224fe5755e53d2e0783a5bc0c1
Amon Victor <amon.victor.develops@gmail.com>
2025-07-30 12:57:39 +0300

    added deployed cors options

--- server.ts
+++ server.ts
@@ -12,8 +12,8 @@ const app = express();
 
 app.use(
   cors({
-    // origin: "https://notely-app-tau.vercel.app"
-    origin: "http://localhost:5173",
+    origin: "https://notely-app-tau.vercel.app"
+    // origin: "http://localhost:5173",
   }),
 );
 app.use(json());
